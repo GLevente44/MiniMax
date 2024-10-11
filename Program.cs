9b6bb654c37738cde8500eb5cac254ef5d6a1c46
Levente <gecsel@kkszki.hu>
2024-10-11 11:16:17 +0200

    Checking Minimum and maximum numbers

--- Program.cs
+++ Program.cs
@@ -10,6 +10,44 @@ namespace MiniMax
     {
         static void Main(string[] args)
         {
+            int db = 5;
+            Random random = new Random();
+            int[] lista = new int[db];
+
+            for (int i = 0; i < db; i++)
+            {
+
+                lista[i] = random.Next(1, 101);
+
+            }
+
+            Console.WriteLine("A tömb számai: ");
+            foreach (int szam in lista)
+            {
+
+                Console.Write(szam + ", ");
+
+            }
+
+            Console.WriteLine();
+
+            int minszam = lista[0];
+            int maxszam = lista[0];
+
+            for (int i = 1; i < lista.Length; i++)
+            {
+                if (lista[i] < minszam)
+                {
+                    minszam = lista[i];
+                }
+                if (lista[i] > maxszam)
+                {
+                    maxszam = lista[i];
+                }
+            }
+
+            Console.WriteLine("A legkisebb elem: " + minszam);
+            Console.WriteLine("A legnagyobb elem: " + maxszam);
         }
     }
 }
